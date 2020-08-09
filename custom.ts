10f2bcaba661d7fa74aa9d97dacca5d41f64264d
Christian McCabe <seeward@gmail.com>
2020-08-09 14:44:02 +0200

    hair flip

--- custom.ts
+++ custom.ts
@@ -274,66 +274,66 @@ class Disciple {
         . . . f f f . . . . . . . . 
         `]
     private walkLeft: Image[] = [img`
-        . . . . . f f f f . . . . . 
-        . . . f f e e e e f f . . . 
-        . . f e e e e e e e e f . . 
-        . . f e e e e e e e e f . . 
-        . f e d e e e e e e e e f . 
-        f e d d e e e e e e e e f . 
-        . f d f d d e e e e e e f . 
-        . f d f d d e e e d e e f . 
-        . . f d d d d d d d e f . . 
-        . . . f d d d d d d f . . . 
-        . . . . f f f f f f . . . . 
-        . . . f 2 2 f 2 2 2 f . . . 
-        . . . f 2 f f f 2 2 f . . . 
-        . . . f 2 f d f 2 2 f . . . 
-        . . . . f f d f 2 2 f . . . 
-        . . . . f f d f f f . . . . 
-        . . . f 8 f f f 8 f . . . . 
-        . . . f 8 8 8 8 8 f . . . . 
-        . . . . f f f f f . . . . . 
-        `,img`
-            . . . . . . . . . . . . . .
-            . . . . . f f f f . . . . .
-            . . . f f e e e e f f . . .
-            . . f e e e e e e e e f . .
-            . . f e e e e e e e e f . .
-            . f e d e e e e e e e e f .
-            f e d d e e e e e e e e f .
-            . f d f d d e e e e e e f .
-            . f d f d d e e e d e e f .
-            . . f d d d d d d d e f . .
-            . . f f d d d d d d f . . .
-            . . f d f f f f f f d f . .
-            . f d d 2 2 2 2 2 d d d f .
-            . . f 2 2 2 2 2 2 d d d f .
-            . f e f 8 8 f 8 8 d d f . .
-            f e e e f 8 f 8 8 8 f f . .
-            . f e e e f . f f f e e f .
-            . . f e f . . . f e e f . .
-            . . . f . . . . . f f . . .
-        `,img`
-            . . . . . f f f f . . . . .
-            . . . f f e e e e f f . . .
-            . . f e e e e e e e e f . .
-            . . f e e e e e e e e f . .
-            . f e d e e e e e e e e f .
-            f e d d e e e e e e e e f .
-            . f d f d d e e e e e e f .
-            . f d f d d e e e d e e f .
-            . . f d d d d d d d e f . .
-            . . . f d d d d d d f . . .
-            . . . . f f f f f f . . . .
-            . . . f 2 2 f 2 2 2 f . . .
-            . . . f 2 f f f 2 2 f . . .
-            . . . f 2 f d f 2 2 f . . .
-            . . . . f f d f 2 2 f . . .
-            . . . . f f d f f f . . . .
-            . . . f 8 f f f 8 f . . . .
-            . . . f 8 8 8 8 8 f . . . .
-            . . . . f f f f f . . . . .
-        `,img`
+        . . . . . f f f f . . . . .
+        . . . f f e e e e f f . . .
+        . . f e e e e e e e e f . .
+        . f e e e e e e e e e f . .
+        f e e d e e e e e e e e f .
+        f e d d e e e e e e e e f .
+        . f d f d d e e e e e e f .
+        . f d f d d e e e d e e f .
+        . . f d d d d d d d e f . .
+        . . . f d d d d d d f . . .
+        . . . . f f f f f f . . . .
+        . . . f 2 2 f 2 2 2 f . . .
+        . . . f 2 f f f 2 2 f . . .
+        . . . f 2 f d f 2 2 f . . .
+        . . . . f f d f 2 2 f . . .
+        . . . . f f d f f f . . . .
+        . . . f 8 f f f 8 f . . . .
+        . . . f 8 8 8 8 8 f . . . .
+        . . . . f f f f f . . . . .
+    `,img`
+        . . . . . . . . . . . . . .
+        . . . . . f f f f . . . . .
+        . . . f f e e e e f f . . .
+        . . f e e e e e e e e f . .
+        . f e e e e e e e e e f . .
+        f e e d e e e e e e e e f .
+        f e d d e e e e e e e e f .
+        . f d f d d e e e e e e f .
+        . f d f d d e e e d e e f .
+        . . f d d d d d d d e f . .
+        . . f f d d d d d d f . . .
+        . . f d f f f f f f d f . .
+        . f d d 2 2 2 2 2 d d d f .
+        . . f 2 2 2 2 2 2 d d d f .
+        . f e f 8 8 f 8 8 d d f . .
+        f e e e f 8 f 8 8 8 f f . .
+        . f e e e f . f f f e e f .
+        . . f e f . . . f e e f . .
+        . . . f . . . . . f f . . .
+    `,img`
+        . . . . . f f f f . . . . .
+        . . . f f e e e e f f . . .
+        . f f e e e e e e e e f . .
+        f e e e e e e e e e e f . .
+        f e d d e e e e e e e e f .
+        f d d d e e e e e e e e f .
+        . f d f d d e e e e e e f .
+        . f d f d d e e e d e e f .
+        . . f d d d d d d d e f . .
+        . . . f d d d d d d f . . .
+        . . . . f f f f f f . . . .
+        . . . f 2 2 f 2 2 2 f . . .
+        . . . f 2 f f f 2 2 f . . .
+        . . . f 2 f d f 2 2 f . . .
+        . . . . f f d f 2 2 f . . .
+        . . . . f f d f f f . . . .
+        . . . f 8 f f f 8 f . . . .
+        . . . f 8 8 8 8 8 f . . . .
+        . . . . f f f f f . . . . .
+    `,img`
             . . . . . . . . . . . . . .
             . . . . . f f f f . . . . .
             . . . f f e e e e f f . . .
